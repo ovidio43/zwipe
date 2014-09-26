d7ccceed30ebdfd45fa78cb01efda1df1682abf3
Edwin Condori <ovidio43@gmail.com>
2014-09-26 08:03:26 -0400

    add theme files

---
+++ wp-content/themes/zwipeTheme/footer.php
@@ -0,0 +1,20 @@
+    <footer>
+    </footer>
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>window.jQuery || document.write('<script src="<?php echo get_template_directory_uri(); ?>/js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/vendor/bootstrap.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/bxslider/jquery.bxslider.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/plugins.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/js/main.js"></script>
+    <?php wp_footer(); ?>
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+        (function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
+        function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
+        e=o.createElement(i);r=o.getElementsByTagName(i)[0];
+        e.src='//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
+        ga('create','UA-XXXXX-X');ga('send','pageview');
+    </script>
+    </body>
+</html>
